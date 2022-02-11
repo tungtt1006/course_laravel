0a0adfbd55f899109ff34d11b7ae87e4e74b439c
TungThinkPadT460s <toilatung52@gmail.com>
2022-02-11 17:47:45 +0700

    Test

---
+++ database/migrations/2022_01_08_104150_create_users__table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 50);
+            $table->char('email', 50);
+            $table->char('password', 60);
+            $table->char('address', 500);
+            $table->char('phone', 11);
+            $table->char('photo', 255);
+            $table->char('remember_token', 255);
+            $table->tinyInteger('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_');
+    }
+}
