7639f1436eb187cdac373018e47d7c56f87f99ed
phxtung <tungpx@symper.vn>
2022-05-15 15:48:51 +0700

    Set up bootstrap, jquery by npm

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,7 +7,6 @@
  */
 
  try {
-    window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
--- resources/views/admin-layout/layout.blade.php
+++ resources/views/admin-layout/layout.blade.php
@@ -6,8 +6,7 @@
     <title>Kourse | Admin</title>
     <meta name="description" content="Course | Admin">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- <script src="{{ asset('js/app.js') }}"></script> -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}">
 </head>
@@ -35,7 +34,6 @@
             </div>
         </div>
     </div>
-<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script> -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
