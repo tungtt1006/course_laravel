8911c16c50ed0edf4be4039cf76c08e7a57b6f77
zinza <toilatung52@gmail.com>
2022-05-20 15:20:50 +0700

    Update get product follow category

--- app/Http/Controllers/Client/CategoryProductController.php
+++ app/Http/Controllers/Client/CategoryProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryProductController extends Controller
 {
@@ -12,11 +13,13 @@ class CategoryProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Category $category)
+    public function index(Request $request, Category $category)
     {
+        $type = $request->input('type', 'id');
+        $order = $request->input('order', 'desc');
         return [
-            'products' => $category->products()->isDisplayed()->paginate(9),
-            'category' => $category
+            'products' => $category->products()->isDisplayed()->orderBy($type, $order)->simplePaginate(9),
+            'category' => $category,
         ];
     }
 }
